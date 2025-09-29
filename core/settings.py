708acec5b5ab0eb976ba99d29d1d8e76b5fb4f0e
Francois Wautier <francois@wautier.eu>
2025-09-29 20:24:23 +0700

    Add STATICFILES_DIRS pointing to assets directory

--- core/settings.py
+++ core/settings.py
@@ -185,7 +185,7 @@ USE_TZ = False
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)  # Commented out to avoid conflict with STATIC_ROOT
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
