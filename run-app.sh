b77f2e0f769abe4124937833412deed09a127d1d
Francois Wautier <francois@wautier.eu>
2025-09-30 17:43:20 +0700

    fix: Only run migrations and create superuser on first run

--- run-app.sh
+++ run-app.sh
@@ -3,14 +3,18 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
+# Check if this is the first run
+if [ ! -f /app/.first_run_complete ]; then
+  # Apply database migrations
+  python manage.py makemigrations
+  python manage.py migrate
 
-# Generate initial data
+  # Create a superuser
+  python manage.py createsuperuser_with_password --username ${DJANGO_SUPERUSER_USERNAME} --password ${DJANGO_SUPERUSER_PASSWORD}
 
-# Create a superuser
-python manage.py createsuperuser_with_password --username ${DJANGO_SUPERUSER_USERNAME} --password ${DJANGO_SUPERUSER_PASSWORD}
+  # Create the flag file
+  touch /app/.first_run_complete
+fi
 
 # Start the Gunicorn server
 exec gunicorn --config gunicorn-cfg.py core.asgi
